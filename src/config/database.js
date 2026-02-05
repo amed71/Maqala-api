fc428db699861ef8ee35dd1d67c731c35bdbe22b
hasani <ahmad2007hasani@gmail.com>
2026-02-05 22:30:05 +0700

    fix: force pg dialect module

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,17 @@
 const { Sequelize } = require('sequelize');
+const pg = require('pg'); // ⬅ tambahkan ini
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectModule: pg, // ⬅ tambahkan ini (penting!)
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false
     }
   },
-  logging: console.log,
+  logging: false,
   pool: {
     max: 5,
     min: 0,
